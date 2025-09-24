980e595d4fe0f6b835a6253faef3f128ed22502f
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-09-24 12:23:57 +0900

    :wrench: prettier導入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,55 +22,58 @@ function App() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="田中太郎"
-        {...register("username", {
-          required: {
-            value: true,
-            message: "ユーザー名の入力は必須です。",
-          },
-        })}
-      />
-      {errors.username && <span>{errors.username.message}</span>}
+    <main>
+      <h1>サインアップ</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="田中太郎"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "ユーザー名の入力は必須です。",
+            },
+          })}
+        />
+        {errors.username && <span>{errors.username.message}</span>}
 
-      <input
-        type="email"
-        placeholder="email@example.com"
-        {...register("email", {
-          required: {
-            value: true,
-            message: "メールアドレスの入力は必須です。",
-          },
-          pattern: {
-            value: EMAIL_REGEX,
-            message:
-              "入力内容に不備があるようです。メールアドレスを正しく入力してください。",
-          },
-        })}
-      />
-      {errors.email && <span>{errors.email.message}</span>}
+        <input
+          type="email"
+          placeholder="email@example.com"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "メールアドレスの入力は必須です。",
+            },
+            pattern: {
+              value: EMAIL_REGEX,
+              message:
+                "入力内容に不備があるようです。メールアドレスを正しく入力してください。",
+            },
+          })}
+        />
+        {errors.email && <span>{errors.email.message}</span>}
 
-      <input
-        type={showPassword ? "text" : "password"}
-        {...register("password", {
-          required: { value: true, message: "パスワードの入力は必須です。" },
-          minLength: {
-            value: 6,
-            message:
-              "入力内容に不備があるようです。パスワードは6文字以上で入力してください。",
-          },
-        })}
-      />
-      {errors.password && <span>{errors.password.message}</span>}
+        <input
+          type={showPassword ? "text" : "password"}
+          {...register("password", {
+            required: { value: true, message: "パスワードの入力は必須です。" },
+            minLength: {
+              value: 6,
+              message:
+                "入力内容に不備があるようです。パスワードは6文字以上で入力してください。",
+            },
+          })}
+        />
+        {errors.password && <span>{errors.password.message}</span>}
 
-      <button type="button" onClick={() => setShowPassword((prev) => !prev)}>
-        {showPassword ? <Eye /> : <EyeClosed />}
-      </button>
+        <button type="button" onClick={() => setShowPassword((prev) => !prev)}>
+          {showPassword ? <Eye /> : <EyeClosed />}
+        </button>
 
-      <button>登録</button>
-    </form>
+        <button>登録</button>
+      </form>
+    </main>
   );
 }
 
